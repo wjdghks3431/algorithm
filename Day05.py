45626c43fc591dbb6c19501123989835fbb25fb0
wjdghks3431 <wjdghks3431@naver.com>
2022-11-20 22:21:24 +0900

    1차원 배열

---
+++ Day05.py
@@ -0,0 +1,31 @@
+# 1.
+def solve(a):
+    return sum(a)
+
+
+def solve(a):
+    total = 0
+    for x in a:
+        total += x
+    return total
+
+# 2.
+
+
+def d(n):
+    # 생성자 n을 이용해 d(n)을 만드는 수식
+    n = n+sum(map(int, str(n)))
+    return n
+
+
+#  셀프 넘버가 아닌 수들 (생성자가 있는 수들)이 들어갈 집합)
+nonSelfNum = set()
+
+# nonSelfNum 집합에 들어갈 수들을 찾아 넣기
+for i in range(1, 10001):
+    nonSelfNum.add(d(i))
+
+#  셀프 넘버들을 출력하기
+for j in range(1, 10001):
+    if j not in nonSelfNum:
+        print(j)
